7b9452bd1927a00c36970bc5e346a1d4abdbda51
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-04-06 12:00:30 -0400

    added different colors to the buttons, added display, futzed with where the number goes in display

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
+
+export enum ButtonType {
+    Number,
+    Operation
+};
+
 type Props = {
     label: string;
     position?: [x: number, y:number];
     height?: number;
+    type?: ButtonType;
     width?: number;
 }
 
 const StyledButton = styled.button`
-    background: #E48900;
+    background: #727171;
     border: none;
     border-radius: 8px;
+    color: white;
     font-size: 24px;
 `;
 
-const Button: React.FC<Props> = ({ label, position, height, width }) => {
+const Button: React.FC<Props> = ({ label, position, height, type = ButtonType.Operation, width }) => {
     const styles:React.CSSProperties = {};
     if(position) {
        styles.gridColumnStart = position[0] + 1;
@@ -28,6 +36,11 @@ const Button: React.FC<Props> = ({ label, position, height, width }) => {
 
     if(height){
         styles.gridRowEnd = `span ${height}`;
+    }
+
+    if(type === ButtonType.Number){
+        styles.color = '#000';
+        styles.background = '#E48900';
     }
 
     return(
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "./Button";
+import Button, { ButtonType } from "./Button";
 
 const Container = styled.div `
     
@@ -10,28 +10,38 @@ const Grid = styled.div`
     display: grid;
     grid-gap: 10px;
     grid-template-columns: repeat(4, 80px);
-    grid-template-rows: repeat(5, 80px);
+    grid-template-rows: 120px repeat(5, 80px);
+`;
+
+const Display = styled.div`
+    background: #fff;
+    border-radius: 8px;
+    font-size: 48px;
+    grid-column-end: span 4;
+    padding: 30px 24px;
+    text-align: right;
 `;
 
 const Calculator: React.FC<{}> = () => {
     return(
         <Container>
             <Grid>
-                <Button label="AC" position={[0,0]} width={2}></Button>
-                <Button label="Oops" position={[2,0]} width={2}></Button>
-                <Button label="+" position={[3,1]}></Button>
-                <Button label="-" position={[3,2]}></Button>
-                <Button label="=" position={[3,3]} height={2}></Button>
-                <Button label="9" position={[2,1]}></Button>
-                <Button label="8" position={[1,1]}></Button>
-                <Button label="7" position={[0,1]}></Button>
-                <Button label="6" position={[2,2]}></Button>
-                <Button label="5" position={[1,2]}></Button>
-                <Button label="4" position={[0,2]}></Button>
-                <Button label="3" position={[2,3]}></Button>
-                <Button label="2" position={[1,3]}></Button>
-                <Button label="1" position={[0,3]}></Button>
-                <Button label="0" position={[0,4]} width={3}></Button>
+                <Display>42</Display>
+                <Button label="AC" position={[0,1]} width={2}></Button>
+                <Button label="Oops" position={[2,1]} width={2}></Button>
+                <Button label="+" position={[3,2]}></Button>
+                <Button label="-" position={[3,3]}></Button>
+                <Button label="=" position={[3,4]} height={2}></Button>
+                <Button type={ButtonType.Number} label="9" position={[2,2]}></Button>
+                <Button type={ButtonType.Number} label="8" position={[1,2]}></Button>
+                <Button type={ButtonType.Number} label="7" position={[0,2]}></Button>
+                <Button type={ButtonType.Number} label="6" position={[2,3]}></Button>
+                <Button type={ButtonType.Number} label="5" position={[1,3]}></Button>
+                <Button type={ButtonType.Number} label="4" position={[0,3]}></Button>
+                <Button type={ButtonType.Number} label="3" position={[2,4]}></Button>
+                <Button type={ButtonType.Number} label="2" position={[1,4]}></Button>
+                <Button type={ButtonType.Number} label="1" position={[0,4]}></Button>
+                <Button type={ButtonType.Number} label="0" position={[0,5]} width={3}></Button>
             </Grid>
         </Container>
     )
